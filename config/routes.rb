d69dee64420e38407ade54cb993e78431614f5b6
Kenji kojima <k.kojima09211986@gmail.com>
2019-11-17 21:31:01 +0900

    refs #8 静的ページ生成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'static_pages/home'
+  get 'static_pages/help'
   root to: 'tasks#index'
   get 'login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
